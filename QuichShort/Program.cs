a65645a30ccbd78ae53981fb857173deb063485b
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2022-09-25 15:36:30 +0700

    Created array

--- QuichShort/Program.cs
+++ QuichShort/Program.cs
@@ -4,9 +4,10 @@ namespace QuichShort
 {
     class Program
     {
+        private int[] arr = new int[20];
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
